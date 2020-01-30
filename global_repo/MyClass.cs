c656c7a58126249483714f8b0c2a6fe62400dece
Your Name <you@example.com>
2020-01-30 10:02:45 -0800

    init

---
+++ global_repo/MyClass.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by Ranorex
+ * User: vagrant
+ * Date: 1/29/2020
+ * Time: 12:28 PM
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+
+using Ranorex;
+
+namespace global_repo
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		// TODO: Add Code to call testing functions - for example:	
+		// Automation.Start()
+	}
+}
